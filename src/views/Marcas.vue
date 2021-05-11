3846b9e75a3d17ea86b49928f53ebb0d9fa8bf67
Enrique <eamrmoreno@gmail.com>
2021-05-11 16:46:47 -0600

    esta es una prueba para rama remota

--- src/views/Marcas.vue
+++ src/views/Marcas.vue
@@ -5,7 +5,7 @@
                 <div class="card">
                     <div class="card-header">
                         <h6 class="text-uppercase mb-0" style="display: inline-block;">
-                        Marcas
+                        Marcasotas
                         </h6>
                         <b-button
                             v-b-modal.modalAgregarMar
